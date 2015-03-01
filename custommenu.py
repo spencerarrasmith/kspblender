bc74619c1e78fea479a4696c01a67dabf92761a6
Scrumilation <rod.junger@hotmail.com>
2015-02-28 23:30:20 -0300

    PEP8 fixes
    Beautiful code is easier to understand.

--- custommenu.py
+++ custommenu.py
@@ -1,34 +1,37 @@
-import bpy, time, ksparser
+import bpy
+import time
+import ksparser
+
 
 class SelectShipOperator(bpy.types.Operator):
     bl_idname = "object.select_ship"
     bl_label = "Select Ship"
-    
-    def execute(self,context):
+
+    def execute(self, context):
         scn = bpy.context.scene
         selected = bpy.context.selected_objects
         shippart = selected.pop(0)
-        bpy.ops.object.select_all(action = 'DESELECT')
+        bpy.ops.object.select_all(action='DESELECT')
         shippart.select = True
-        
+
         for obj in bpy.data.objects:
             if not obj.parent:
                 if obj["ship"] == shippart["ship"]:
                     obj.select = True
-                    
+
         return {'FINISHED'}
-        
+
 
 class DeletePartOperator(bpy.types.Operator):
     bl_idname = "object.delete_part"
     bl_label = "Delete Part"
 
-    def execute(self,context):
+    def execute(self, context):
         scn = bpy.context.scene
         kill = bpy.context.selected_objects
 
         for obj in kill:
-            if obj.parent == None:
+            if obj.parent is None:
                 queue = [obj]
 
                 while queue:
@@ -39,78 +42,81 @@ class DeletePartOperator(bpy.types.Operator):
                     scn.objects.active = current
                     for child in current.children:
                         queue.append(child)
-                        
-                bpy.ops.object.delete(use_global = False)
+
+                bpy.ops.object.delete(use_global=False)
         return {'FINISHED'}
-    
+
+
 class ToggleDeployOperator(bpy.types.Operator):
     bl_idname = "object.toggle_deploy"
     bl_label = "Toggle Deploy"
 
-    def execute(self,context):
+    def execute(self, context):
         scn = bpy.context.scene
         toggle = bpy.context.selected_objects
         selection = toggle
-        
+
         while toggle:
             curobj = toggle.pop(0)
             for child in curobj.children:
                 toggle.append(child)
-                
+
             scn.objects.active = curobj
             curobj.select = True
             if curobj.animation_data:
                 for track in curobj.animation_data.nla_tracks:
                     for strip in track.strips:
                         strip.use_reverse = not(strip.use_reverse)
-                        
+
             curobj.select = False
-        bpy.ops.object.select_all(action = 'DESELECT')
-        bpy.context.scene.frame_current=1
-        bpy.context.scene.frame_current=0
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.context.scene.frame_current = 1
+        bpy.context.scene.frame_current = 0
         for object in selection:
             object.select = True
         return {'FINISHED'}
-    
+
+
 class ToggleEditableOperator(bpy.types.Operator):
     bl_idname = "object.toggle_editable"
     bl_label = "Toggle Editable"
 
-    def execute(self,context):
+    def execute(self, context):
         scn = bpy.context.scene
         editable = bpy.context.selected_objects
-        
+
         while editable:
             curobj = editable.pop(0)
             for child in curobj.children:
                 editable.append(child)
-                
-            if curobj.type == "MESH" and curobj.hide == False:
+
+            if curobj.type == "MESH" and curobj.hide is False:
                 scn.objects.active = curobj
                 curobj.select = True
                 curobj.hide_select = not(curobj.hide_select)
-                
+
         return {'FINISHED'}
+
 
 class ImportCraftOperator(bpy.types.Operator):
     bl_idname = "object.import_craft"
     bl_label = "Import Craft"
-   
-    def execute(self,context):
+
+    def execute(self, context):
         ksparser.main("Kerbal 2.craft")
         return {'FINISHED'}
 
 #class LoadFlagPartOperator(bpy.types.Operator):
 #    bl_idname = "object.load_flag"
 #    bl_label = "Load Flag"
-    
+
     #filename = StringProperty(name="Flag Image", subtype="FILE_PATH")
-    
-    
+
+
 #    def execute(self,context):
 #        loadflag.main()
 #        return {'FINISHED'}
-    
+
 # EnableEditingOperator
 # DisableEditingOperator
 # ChangeSymmetryOperator
@@ -126,11 +132,11 @@ bpy.utils.register_class(ToggleEditableOperator)
 #bpy.utils.register_class(ImportCraftOperator)
 #bpy.utils.register_class(LoadFlagOperator)
 
+
 class CustomMenu(bpy.types.Menu):
     bl_idname = "OBJECT_MT_CustomMenu"
     bl_label = "Custom Menu"
-    
-    
+
     def draw(self, context):
         layout = self.layout
         layout.operator("object.shade_smooth", text="Shade Smooth")
@@ -147,8 +153,8 @@ class CustomMenu(bpy.types.Menu):
         layout.operator("object.toggle_deploy", text="Toggle Deploy")
         layout.operator("object.toggle_editable", text="Toggle Editable")
         #layout.operator("object.import_craft", text="Import Craft")
-        
-        
+
+
 bpy.utils.register_class(CustomMenu)
 
 km = bpy.context.window_manager.keyconfigs.default.keymaps['3D View']
--- joinall.py
+++ joinall.py
@@ -1,19 +1,20 @@
 import bpy
 
+
 def main(ship):
     scn = bpy.context.scene
     mergelist = []
-    
+
     for obj in scn.objects:
         print(obj.name)
         if obj.type == 'MESH' and "KSP" not in obj.name:
             if obj['ship'] == ship and not obj.hide:
                 mergelist.append(obj)
         else:
-            print("Skipped ",obj.name)
-    
+            print("Skipped ", obj.name)
+
     while len(mergelist) > 1:
-        bpy.ops.object.select_all(action = 'DESELECT') 
+        bpy.ops.object.select_all(action='DESELECT')
         merge1 = mergelist.pop()
         merge2 = mergelist.pop()
         scn.objects.active = merge1
@@ -24,43 +25,42 @@ def main(ship):
         modname = merge1.modifiers[0].name
         bpy.ops.object.modifier_apply(apply_as='DATA', modifier=modname)
         mergelist.append(bpy.context.active_object)
-    
+
     result = bpy.context.active_object
     result.select = True
     bpy.ops.object.mode_set(mode='EDIT')
-    bpy.ops.mesh.remove_doubles(threshold = 0.001)
-    bpy.ops.mesh.normals_make_consistent(inside = False)
+    bpy.ops.mesh.remove_doubles(threshold=0.001)
+    bpy.ops.mesh.normals_make_consistent(inside=False)
     bpy.ops.object.mode_set(mode='OBJECT')
     bpy.ops.object.parent_clear(type='CLEAR')
     result.name = "RESULT"
-    result.scale = (10,10,10)
-    
-    
+    result.scale = (10, 10, 10)
+
+
 def alt(ship):
     scn = bpy.context.scene
     mergelist = []
-    
+
     for obj in scn.objects:
         print(obj.name)
         if obj.type == 'MESH' and "KSP" not in obj.name:
             if obj['ship'] == ship and not obj.hide:
                 mergelist.append(obj)
         else:
-            print("Skipped ",obj.name)
-    
-    bpy.ops.object.select_all(action = 'DESELECT')         
+            print("Skipped ", obj.name)
+
+    bpy.ops.object.select_all(action='DESELECT')
     for obj in mergelist:
         obj.select = True
-    
+
     bpy.ops.object.join()
     bpy.ops.object.parent_clear(type='CLEAR')
     result.name = "RESULT"
-    result.scale = (10,10,10)
+    result.scale = (10, 10, 10)
     bpy.ops.object.mode_set(mode='EDIT')
-    bpy.ops.mesh.remove_doubles(threshold = 0.001)
-    bpy.ops.mesh.normals_make_consistent(inside = False)
+    bpy.ops.mesh.remove_doubles(threshold=0.001)
+    bpy.ops.mesh.normals_make_consistent(inside=False)
     bpy.ops.object.mode_set(mode='OBJECT')
-    
-        
+
 
 alt('Kerbal 2X')
